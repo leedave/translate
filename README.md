0e209010b3516d92e035e4934dbdd4287a505c30
David Lee <leed@leed.ch>
2018-01-01 23:00:06 +0100

    INIT

---
+++ README.md
@@ -0,0 +1,7 @@
+# Translation Component #
+
+This is a library to handle translations. I choose to use csv files as source, because 
+these can easily be edited using Libreoffice or MS Office
+
+Currently this provides translations for use in PHP and in Views (leedch/view). 
+In future it should also support Javascript
---
+++ src/Translate.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Leedch\Translate;
+
+/**
+ * Description of Translation
+ *
+ * @author leed
+ */
+class Translate
+{
+    private $translations = [];
+    private static $instance;
+    
+    public static function getInstance(){
+        if(!self::$instance){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+    
+    public function loadTranslations(array $arrFiles)
+    {
+        $rows = [];
+        foreach ($arrFiles as $file) {
+            $filePath = leedch_pathTranslations . $file;
+            if (!file_exists($filePath)) {
+                continue;
+            }
+            $file = fopen($filePath, "r");
+            while (!feof($file)) {
+                $fRow = fgetcsv($file, 0, ",");
+                if (count($fRow) < 2) {
+                    continue;
+                }
+                $rows[$fRow[0]] = $fRow[1];
+            }
+        }
+        $this->translations = $rows;
+    }
+    
+    public function getTranslations() : array
+    {
+        return $this->translations;
+    }
+    
+    /**
+     * Get the current Translation
+     * 
+     * @param string $code
+     * @return string
+     */
+    public static function __(string $code) : string
+    {
+        $t = self::getInstance();
+        if (!isset($t->translations[$code])) {
+            return $code;
+        }
+        return $t->translations[$code];
+    }
+}
